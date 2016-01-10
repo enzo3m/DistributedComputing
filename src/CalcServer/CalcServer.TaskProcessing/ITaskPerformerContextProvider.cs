7ac6cb8d8fd44bb3e4960449a48d8f884db367f6
enzo3m <enzo3m@users.noreply.github.com>
2016-01-10 15:02:39 +0100

    Move interface definition in a separate code file
    ITaskPerformerContextProvider interface definition has been moved in a
    separate code file.

---
+++ src/CalcServer/CalcServer.TaskProcessing/ITaskPerformerContextProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalcServer.TaskProcessing
+{
+    /// <summary>
+    /// Interfaccia di sola lettura per accedere ad un insieme di contesti di elaborazione, ognuno dei quali è associato
+    /// al nome completo ed alla versione della classe che lo implementa.
+    /// </summary>
+    public interface ITaskPerformerContextProvider
+    {
+        /// <summary>
+        /// L'implementazione di questo metodo deve provare ad ottenere il contesto di elaborazione associato alla classe
+        /// specificata, restituendo true se tale classe esiste e pertanto è possibile eseguire il task, altrimenti false
+        /// se non esiste e quindi non è possibile eseguire task.
+        /// </summary>
+        /// <param name="className">nome completo della classe di cui si vuole ottenere il contesto di elaborazione</param>
+        /// <param name="classVersion">versione della classe di cui si vuole ottenere il contesto di elaborazione</param>
+        /// <param name="context">
+        /// Quando termina, questo metodo deve restituire il contesto di elaborazione associato alla classe specificata
+        /// nel caso in cui la classe venga trovata; in caso contrario deve restituire il contesto predefinito, che non
+        /// esegue alcuna elaborazione.
+        /// </param>
+        /// <returns>
+        /// true, se l'oggetto che implementa l'interfaccia ITaskPerformerContextProvider contiene il contesto cercato per
+        /// l'elaborazione e specificato dal nome completo e dalla versione della classe, altrimenti false
+        /// </returns>
+        bool TryGetContext(string className, string classVersion, out TaskPerformerContext context);
+
+        /// <summary>
+        /// L'implementazione di questo metodo deve controllare se esiste ed è abilitato il contesto di elaborazione
+        /// associato alla classe specificata, restituendo true se tale classe esiste e pertanto è possibile eseguire
+        /// il task, altrimenti false se non esiste e quindi non è possibile eseguire task.
+        /// </summary>
+        /// <param name="className">nome completo della classe di cui si vuole verificare il contesto di elaborazione</param>
+        /// <param name="classVersion">versione della classe di cui si vuole verificare il contesto di elaborazione</param>
+        /// <returns>
+        /// true, se l'oggetto che implementa l'interfaccia ITaskPerformerContextProvider contiene il contesto cercato
+        /// per l'elaborazione e specificato dal nome completo e dalla versione della classe, altrimenti false
+        /// </returns>
+        bool IsContextEnabled(string className, string classVersion);
+    }
+}
